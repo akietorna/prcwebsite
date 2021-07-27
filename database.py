d7260bc4c574e264c8796cde6ef8fba55ecacb16
Agbodjive Etorna k <akietorna@gmail.com>
2021-07-27 05:47:21 +0000

    update

--- database.py
+++ database.py
@@ -9,10 +9,10 @@ import MySQLdb.cursors
 #app.config['SECRET_KEY'] = 'hispresence123@'
 
 def connection():
-    connect = MySQLdb.connect(host = "ekagbodjive.mysql.pythonanywhere-services.com",
-                              user = "ekagbodjive",
-                              passwd = "hispresence123@",
-                              db = "prcwebsite")
+    connect = MySQLdb.connect(HOST = "ekagbodjive.mysql.pythonanywhere-services.com",
+                              USER = "ekagbodjive",
+                              PASSWD = "hispresence123@",
+                              DB = "prcwebsite")
 # innitializing the cursor 
     curs = connect.cursor()
 
